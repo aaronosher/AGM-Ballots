cf3533776293809152d8006b8058283c8236e627
Aaron Osher <aaron@oshers.com>
2018-02-15 20:45:34 +0000

    Add Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import {SuiModule} from 'ng2-semantic-ui';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BallotComponent } from './pages/ballot/ballot.component';
+import { TallyComponent } from './pages/tally/tally.component';
+import { ResultsComponent } from './pages/results/results.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    BallotComponent,
+    TallyComponent,
+    ResultsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
