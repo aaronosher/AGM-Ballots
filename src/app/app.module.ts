24359705f0ccc306df31239c63b8334da517ec52
Aaron Osher <aaron@oshers.com>
2018-02-16 18:10:22 +0000

    Update AppModule
    Add Ballot Service to providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {SuiModule} from 'ng2-semantic-ui';
 import { AppRouterModule } from './app-router.module';
 
+import { BallotService } from './services/ballot.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
@@ -28,7 +29,7 @@ import { LoginComponent } from './pages/login/login.component';
     SuiModule,
     AppRouterModule
   ],
-  providers: [],
+  providers: [BallotService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
