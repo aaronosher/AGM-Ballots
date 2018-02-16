183d859d156caf4515015956ba76d4ff39decf21
Aaron Osher <aaron@oshers.com>
2018-02-16 17:12:26 +0000

    Add BallotInterface

---
+++ src/app/interfaces/ballot-interface.ts
@@ -0,0 +1,26 @@
+import { BallotType } from '../enums/ballot-type.enum';
+import { BallotOption } from './ballot-option';
+
+/**
+ * BallotInterface is used for each ballot a user may be presented with.
+ */
+export interface BallotInterface {
+  /**
+   * Name of the ballot
+   * @var string
+   */
+  name: string;
+
+  /**
+   * Type of ballot being used. See BallotType enum for details of types
+   * @var BallotType
+   */
+  type: BallotType;
+
+  /**
+   * The options for the ballot
+   * @var BallotOption[] Array of BallotOption interfaces
+   * @var null used when the type is Basic
+   */
+  options: BallotOption[] | null;
+}
